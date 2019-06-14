cb513657984c269ff0d175eef1044f753c3682d7
yangchao <chao.yang9@ymm56.com>
2019-06-14 13:58:04 +0800

    测试改变参数代码

---
+++ DesignPattern/src/com/base/MethodChangeParameter.java
@@ -0,0 +1,36 @@
+package com.base;
+
+/**
+ * 测试在函数中修改参数之后的返回值
+ */
+public class MethodChangeParameter {
+    public static void main(String[] args) {
+        System.out.println("test1=================");
+        int a = 10;
+        System.out.println(a);
+        changeInt(a);
+        System.out.println(a);
+        System.out.println("test2=================");
+        Student student =new Student();
+        changeClass1(student);
+        System.out.println(student.getName()+"+"+student.getAge());
+        System.out.println("test3=================");
+        Student student3 =new Student();
+        changeClass2(student3);
+        System.out.println(student3.getName()+"+"+student3.getAge());
+    }
+
+    public static void changeInt(int a) {
+        a=20;
+    }
+    public static void changeClass1(Student student) {
+        student.setName("yangchao");
+        student.setAge(23);
+    }
+
+    public static void changeClass2(Student student) {
+        Student student2=new Student(24,"小杨");
+        student=student2;
+    }
+
+}
---
+++ DesignPattern/src/com/proxy/Test.java
@@ -0,0 +1,8 @@
+package com.proxy;
+
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("测试commit");
+    }
+}
